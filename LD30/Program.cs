10b17415b1cb0560bfda101ae40c29b6b83d376a
Alex Van Liew <snoozbuster@yahoo.com>
2014-08-22 19:24:41 -0700

    Initial commit

---
+++ LD30/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace LD30
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (MainGame game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
